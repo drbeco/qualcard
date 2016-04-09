b1423080afa4a700fa826d75903ec5a61d1ff83c
Ruben Carlo Benante <rcb@beco.cc>
2016-04-09 20:22:05 -0300

    draft one - indent 4 spaces

--- qualcard.c
+++ qualcard.c
@@ -72,9 +72,9 @@
 #include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
 #include <getopt.h> /* get options from system argc/argv */
 #include <time.h> /* Time and date functions */
+#include <string.h> /* Strings functions definitions */
 
 /* #include <math.h> */ /* Mathematics functions */
-/* #include <string.h> */ /* Strings functions definitions */
 /* #include <assert.h> */ /* Verify assumptions with assert */
 /* #include <dlfcn.h> */ /* Dynamic library */
 /* #include <malloc.h> */ /* Dynamic memory allocation */
@@ -103,10 +103,8 @@
 /* definitions */
 
 /* #define BUILD (20160409.000957) / * * < Build Version Number */
-#define PEREXEC 10 /**< How many cards presented per round (execution) */
-#define NEWEXEC 1 /**< Minimum number of new cards presented per round */
-#define DBEXT ".ex4" /**< Database extension: theme-question-answer.ex4 (example: english-word-definition.ex4) */
-#define CFEXT ".cf4" /**< Configuration file extension: user-qualcard.cf4 */
+#define EXTDB ".ex4" /**< Database extension: theme-question-answer.ex4 (example: english-word-definition.ex4) */
+#define EXTCF ".cf4" /**< Configuration file extension: user-qualcard.cf4 */
 
 /* Debug */
 #ifndef DEBUG /* gcc -DDEBUG=1 */
@@ -117,20 +115,36 @@
 #define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
 
 /* limits */
-#define SBUFF 256 /**< String buffer */
+#define TAMLINE 1500
+#define STRSIZE 256 /**< String buffer size */
+#define PEREXEC 10 /**< How many cards presented per round (execution) */
+#define NEWEXEC 1 /**< Minimum number of new cards presented per round */
 
 /* ---------------------------------------------------------------------- */
 /* globals */
 
 static int verb=0; /**< verbose level, global within the file */
 
+typedef struct scfg /* configuration data struct */
+{
+    int QTDCARD; /* database size */
+    char today[14]; /* yyyymmdd */
+    char user[STRSIZE]; /* user name */
+    char bdados[STRSIZE], config[STRSIZE]; /* filenames: database and configuration */
+    FILE *fbd; /* database file pointer */
+    FILE *fcf; /* config file pointer */
+} tcfg; /* configuration data type */
+
 /* ---------------------------------------------------------------------- */
 /* prototypes */
 
 void help(void); /* print some help */
 void copyr(void); /* print version and copyright information */
-void qualcard_init(void); /* global initialization function */
-void summary(void); /* how many cards to review */
+void qualcard_init(tcfg *cfg); /* global initialization function */
+void summary(tcfg c); /* how many cards to review */
+int bdsize(FILE *fp); /* database size */
+double rand_minmax(double min, double max); /* drawn a number from [min, max[ */
+int newcard(tcfg c, char *card); /* drawn a new card */
 int funcexample(int i, int *o, int *z); /* just an example with complete doxygen fields */
 
 /* ---------------------------------------------------------------------- */
@@ -169,44 +183,111 @@ int funcexample(int i, int *o, int *z); /* just an example with complete doxygen
  */
 int main(int argc, char *argv[])
 {
-  int opt; /* return from getopt() */
+    int opt; /* return from getopt() */
+    int SUMMA=0; /* summary only */
+    time_t lt;
+    struct tm *timeptr;
+    /*int QTDCARD; / * database size * /
+    char today[14]; / * yyyymmdd * /
+    char bdados[STRSIZE], config[STRSIZE], user[STRSIZE]; */
+    tcfg c={0};
+    /* FILE *fbd=NULL, *fcf=NULL; */
+    int l; /* line drawn */
+    int i; /* index, auxiliary */
+    char card[STRSIZE]; /* card drawn */
 
-  qualcard_init(); /* initialization function */
+    IFDEBUG("Starting optarg loop...\n");
 
-  IFDEBUG("Starting optarg loop...\n");
+    /* getopt() configured options:
+     *        -h  help
+     *        -c  copyright & version
+     *        -v  verbose
+     *        -s  status of reviews
+     */
+    opterr = 0;
+    while((opt = getopt(argc, argv, "vhcsu:")) != EOF)
+        switch(opt)
+        {
+            case 'h': /* exit */
+                help();
+                break;
+            case 'c': /* exit */
+                copyr();
+                break;
+            case 'v':
+                verb++;
+                break;
+            case 's':
+                SUMMA=1;
+                break;
+            case 'u':
+                strcpy(c.user, optarg);
+                break;
+            case '?':  /* exit */
+            default:
+                printf("Type\n\t$man %s\nor\n\t$%s -h\nfor help.\n\n", argv[0], argv[0]);
+                return EXIT_FAILURE;
+        }
 
-  /* getopt() configured options:
-   *        -h  help
-   *        -c  copyright & version
-   *        -v  verbose
-   *        -s  status of reviews
-   */
-  opterr = 0;
-  while((opt = getopt(argc, argv, "vhcs")) != EOF)
-    switch(opt)
+    if(verb)
+        printf("Verbose level set at: %d\n", verb);
+    /* ...and we are done */
+
+    qualcard_init(&c); /* initialization function */
+
+    if(SUMMA)
     {
-      case 'h':
-        help();
-        break;
-      case 'c':
-        copyr();
-        break;
-      case 'v':
-        verb++;
-        break;
-      case 's':
-          summary(); /* how many cards to review */
-          break;
-      case '?':
-      default:
-        printf("Type\n\t$man %s\nor\n\t$%s -h\nfor help.\n\n", argv[0], argv[0]);
-        return EXIT_FAILURE;
+        summary(c); /* how many cards to review */
+        exit(EXIT_SUCCESS);
     }
 
-  if(verb)
-    printf("Verbose level set at: %d\n", verb);
-  /* ...and we are done */
-  return EXIT_SUCCESS;
+
+    c.fbd=fopen(c.bdados,"r");
+    if(!c.fbd)
+    {
+        printf("Fail to open database %s.\n", c.bdados);
+        exit(EXIT_FAILURE);
+    }
+    c.QTDCARD=bdsize(c.fbd);
+
+    printf("Today: %s\n", c.today);
+    printf("Number of cards: %d\n", c.QTDCARD);
+    printf("User: %s\n", c.user);
+    printf("DB: %s\n", c.bdados);
+    printf("CF: %s\n", c.config);
+
+
+
+    for(i=0; i<10; i++)
+    {
+        l=newcard(c, card);
+        printf("Card %4d: %s", l, card);
+    }
+
+    if(c.fbd)
+        fclose(c.fbd); /* database file */
+    if(c.fcf)
+        fclose(c.fcf); /* config file */
+
+    return EXIT_SUCCESS;
+}
+
+
+/* database size */
+int bdsize(FILE *fp)
+{
+    char line[TAMLINE];
+    int qtdl=0;
+
+    if(!fp)
+        return 0;
+    fseek(fp, 0, 0 ); /* linha 0 */
+    do
+    {
+        fgets(line, TAMLINE-1, fp);
+        qtdl++;
+    }while(!feof(fp));
+    return --qtdl;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -220,12 +301,101 @@ int main(int argc, char *argv[])
  * @date 2016-04-09
  *
  */
-void summary(void)
+void summary(tcfg c)
 {
-    printf("QualCard - Spaced Repetition\n");
+    printf("QualCard - Spaced Repetition - %s\n", c.today);
+    printf("User: %s\n", c.user);
     printf("%s: you have %d cards to review today.\n", "English", 5);
-    exit(EXIT_SUCCESS);
+    return;
 }
+
+/*
+ * le um novo cartao aleatorio do arquivo
+ * retorna o numero da linha lida (de 0 a FIM-1)
+ * e a string lida
+ */
+int newcard(tcfg c, char *card)
+{
+    int l, i;
+    char line[TAMLINE];
+
+    l=(int)rand_minmax(0.0, c.QTDCARD); /* [= 0, QTDPAD-1 =] */
+    fseek(c.fbd, 0, 0);
+    for(i=0; i<=l; i++)
+        fgets(line, TAMLINE-1, c.fbd);
+    strcpy(card, line);
+    return l; /* linha do arquivo, de 0 a QTDPAD-1 */
+}
+
+/**
+ * \brief Drawn a float number in the given interval [min, max[
+ * \param[in] min Lower number of the interval (closed, inclusive)
+ * \param[in] max Maximum number of the interval (open, exclusive)
+ * \param[out] sorteio The number drawn and returned
+ * @author Ruben Carlo Benante
+ * @version 20160409.000957
+ * @date 2016-04-09
+ */
+double rand_minmax(double min, double max)
+{
+    double sorte;
+
+    sorte = rand();
+    sorte /= ((float)RAND_MAX);
+    sorte *= (max-min);
+    sorte += min;
+    return sorte;
+}
+
+/* ---------------------------------------------------------------------- */
+/**
+ * @ingroup GroupUnique
+ * @brief This function initializes some operations before start
+ * @details Details to be written
+ * @return Void
+ * @todo Need to implement it. Its empty now.
+ * @author Ruben Carlo Benante
+ * @version 20160409.000957
+ * @date 2016-04-09
+ *
+ */
+void qualcard_init(tcfg *cfg) //(char *td, char *bd, char *cf)
+{
+    IFDEBUG("qualcard_init()");
+    /* initialization */
+
+    time_t lt;
+    struct tm *timeptr;
+    /* const char *user=getenv("USER"); */
+//     const char *bdcore="english-word-definition-test";
+    const char *bdcore="english-word-definition";
+
+    lt=time(NULL);
+    timeptr=localtime(&lt);
+    sprintf(cfg->today, "%04d%02d%02d", 1900 + timeptr->tm_year, 1 + timeptr->tm_mon, timeptr->tm_mday);
+
+    srand((unsigned)time(&lt));
+
+    if(cfg->user[0]=='\0')
+        strcpy(cfg->user, getenv("USER"));
+
+    sprintf(cfg->bdados, "%s%s", bdcore, EXTDB);
+    // strcpy(bd, "english-word-definition.ex4");
+    sprintf(cfg->config, "%s-%s%s", cfg->user, bdcore, EXTCF);
+    // strcpy(cf, "beco-english-word-definition-test.cf4");
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
 
 /* ---------------------------------------------------------------------- */
 /**
@@ -240,20 +410,20 @@ void summary(void)
  */
 void help(void)
 {
-  IFDEBUG("help()");
-  printf("%s - %s\n", "qualcard", "Learn cards by Spaced Repetition Method");
-  printf("\nUsage: %s [-h|-v]\n", "qualcard");
-  printf("\nOptions:\n");
-  printf("\t-h,  --help\n\t\tShow this help.\n");
-  printf("\t-c,  --version\n\t\tShow version and copyright information.\n");
-  printf("\t-v,  --verbose\n\t\tSet verbose level (cumulative).\n");
-  printf("\t-s,  --status\n\t\tShow how many cards needs review in each database.\n");
-  printf("\t-u username,  --user username\n\t\tUse the username's profile.\n");
-  /* add more options here */
-  printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
-  printf("\nTodo:\n\tLong options not implemented yet.\n\t-u username not implemented yet.\n");
-  printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
-  exit(EXIT_FAILURE);
+    IFDEBUG("help()");
+    printf("%s - %s\n", "qualcard", "Learn cards by Spaced Repetition Method");
+    printf("\nUsage: %s [-h|-v]\n", "qualcard");
+    printf("\nOptions:\n");
+    printf("\t-h,  --help\n\t\tShow this help.\n");
+    printf("\t-c,  --version\n\t\tShow version and copyright information.\n");
+    printf("\t-v,  --verbose\n\t\tSet verbose level (cumulative).\n");
+    printf("\t-s,  --status\n\t\tShow how many cards needs review in each database.\n");
+    printf("\t-u username,  --user username\n\t\tUse the username's profile.\n");
+    /* add more options here */
+    printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
+    printf("\nTodo:\n\tLong options not implemented yet.\n\t-u username not implemented yet.\n");
+    printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
+    exit(EXIT_FAILURE);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -269,30 +439,11 @@ void help(void)
  */
 void copyr(void)
 {
-  IFDEBUG("copyr()");
-  printf("%s - Version %s\n", "qualcard", VERSION);
-  printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>. This  is  free  software:  you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", 2016, "Ruben Carlo Benante", "rcb@beco.cc");
-  if(verb>3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
-  exit(EXIT_FAILURE);
-}
-
-/* ---------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief This function initializes some operations before start
- * @details Details to be written
- * @return Void
- * @todo Need to implement it. Its empty now.
- * @author Ruben Carlo Benante
- * @version 20160409.000957
- * @date 2016-04-09
- *
- */
-void qualcard_init(void)
-{
-  IFDEBUG("qualcard_init()");
-  /* initialization */
-  return;
+    IFDEBUG("copyr()");
+    printf("%s - Version %s\n", "qualcard", VERSION);
+    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>. This  is  free  software:  you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", 2016, "Ruben Carlo Benante", "rcb@beco.cc");
+    if(verb>3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
+        exit(EXIT_FAILURE);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -333,10 +484,10 @@ void qualcard_init(void)
  */
 int funcexample(int i, int *o, int *z)
 {
-  IFDEBUG("funcexample()");
-  i += *z;
-  *o = (*z)++;
-  return i-4;
+    IFDEBUG("funcexample()");
+    i += *z;
+    *o = (*z)++;
+    return i-4;
 }
 
 /**  @} */
